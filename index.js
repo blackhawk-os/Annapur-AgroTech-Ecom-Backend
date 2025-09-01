06e92c17c2e45d667d1083bd332687f2893f1317
Mehul Gopal Nyachhyon <aventmehul@gmail.com>
2025-09-01 16:22:39 +0545

    Feature: Some APIs for products

--- index.js
+++ index.js
@@ -15,10 +15,7 @@ app.use(express.json());
 app.use(cors());
 
 // Database connection
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser:true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB connected"))
 .catch(err => console.error("MongoDB connection error:", err));
 
