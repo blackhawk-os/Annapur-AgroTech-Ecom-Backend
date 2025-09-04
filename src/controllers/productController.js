554edf9849f7b607c62a3f10380df4a340c24a72
Mehul Gopal Nyachhyon <aventmehul@gmail.com>
2025-09-04 10:22:19 +0545

    Bug fix: Product found with product_Id

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -14,12 +14,18 @@ exports.getAllProducts = async (req, res) => {
 // GET /api/products/:id
 exports.getProductById = async (req, res) => {
   try {
-    const product = await Product.findOne({ id: parseInt(req.params.id) });
+    const product = await Product.findById(req.params.id);
     if (!product){
       return res.status(404).json({ success: false, message: "Product not found" });
 }
     res.json({ success: true, product });
   } catch (error) {
+    if(error.name === 'CastError'){
+        return res.status(400).json({
+            success: false,
+            message: "Invalid product ID format"
+        });
+    }
     res.status(500).json({ success: false, error: error.message });
   }
 };
